3e918d925e54c8217871e519b31b17ba3c11245c
Mardini <mardini1414@gmail.com>
2025-05-25 18:59:26 +0700

    fix compare password

--- utils/bcrypt.go
+++ utils/bcrypt.go
@@ -14,5 +14,5 @@ func HashPassword(password string) string {
 
 func Compare(hashedPassword string, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
-	return err != nil
+	return err == nil
 }
